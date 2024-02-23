2ac52f4e29f875ccc3954583ec493a6814b7c414
Olatunde <122512872+Lawaltunde@users.noreply.github.com>
2024-02-23 16:39:14 +0000

    Created a new Category model that has an Id and description and Category property were added by allowing the class to inherit  from Product model

--- Model/ProductModel.cs
+++ Model/ProductModel.cs
@@ -1,8 +1,16 @@
 namespace AUTHENTICATED_API.Model;
 
-public class ProductModel(){
+public class ProductModel() : ProductModel{
     public int Id{get; set;}
     public decimal price{get; set;}
     public string Name{get; set;} = string.Empty;
     public string Description{get; set;} = string.Empty;
+
+    public ProductModel(int Id, decimal price, string Name, string Description): base(Id, price, Name, Description){
+        this.Id = Id;
+        this.price = price;
+        this.Name = Name;
+        this.Description = Description;
+    }
+
 }
